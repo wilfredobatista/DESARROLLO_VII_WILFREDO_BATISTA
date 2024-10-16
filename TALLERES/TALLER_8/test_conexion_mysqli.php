afaf16ecb98f6d37df681777c5352c2233f5e4bd
wilfredoB <wilfredobatistamela@gmail.com>
2024-10-16 08:49:28 -0500

    TALLER_8 - Paso 1: Configuración inicial de la base de datos con MySQLi y PDO

---
+++ TALLERES/TALLER_8/test_conexion_mysqli.php
@@ -0,0 +1,5 @@
+<?php
+require_once "config_mysqli.php";
+echo "Conexión exitosa a la base de datos con MySQLi.";
+mysqli_close($conn);
+?>
---
+++ TALLERES/TALLER_8/test_conexion_pdo.php
@@ -0,0 +1,6 @@
+<?php
+require_once "config_pdo.php";
+echo "Conexión exitosa a la base de datos con PDO.";
+$pdo = null;
+?>
+     
